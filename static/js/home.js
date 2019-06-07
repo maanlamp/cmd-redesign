e5126d355309a09e6624f53aae35f059a931797e
Tjebbe <tjebbemarchand@icloud.com>
2019-06-07 13:15:14 +0200

    Change call to value

--- static/js/home.js
+++ static/js/home.js
@@ -1,13 +1,9 @@
 import just from '/js/just.js/just.js';
 
-just.plugin(function value () {
-    return this.elements[0].value;
-})
-
 const input = just.select('.input');
 const output = just.select('.output');
 
 input.on('input', () => {
-    const value = input.value();
+    const value = input.value;
     output.innerHTML(marked(value));
 });
