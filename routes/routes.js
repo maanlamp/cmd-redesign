8d1bc720f6f955931fc18828928c069f6a29b6d5
karinliu <karin-liu@hotmail.com>
2019-06-07 12:27:40 +0200

    add home

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,11 @@ function editor (req, res, next) {
 	res.sendFile("html/editor.html", { root: __dirname + "/../static"});
 }
 
+function home(req, res, next){
+	res.sendFile("html/home.html", { root: __dirname + "/../static"});
+}
+
 module.exports = {
-	editor
+	editor,
+	home
 };
