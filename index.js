a94ddc97ffc038ecbd7128355e6b2b5148358fde
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-06-13 12:02:16 +0200

    Removed unnecessary css imports

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/static"));
 app.get("/", routes.home);
-app.get("/:route/:title", (req, res) => routes[req.params.route](req, res));
+app.get("/read/:title", (req, res) => routes.read(req, res));
+app.get("/edit/:title", (req, res) => routes.edit(req, res));
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
