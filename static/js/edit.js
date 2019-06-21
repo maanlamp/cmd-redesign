009503b06f9a9ec8b4f784a2d4507903d2f5ac44
Tjebbe <tjebbemarchand@icloud.com>
2019-06-21 15:50:51 +0200

    Added pages - added components - added header and footer

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,5 +1,7 @@
 import just from "/js/just.js/just.min.js";
-import { getFirstHeadingLinkSafe } from "./parseMarkdown.js";
+import {
+	getFirstHeadingLinkSafe
+} from "./parseMarkdown.js";
 import redirect from "./redirect.js";
 import ResetableTimeout from "./Timeout.js";
 //Remarkable is available through a global script loaded before
@@ -11,12 +13,13 @@ const md = new Remarkable({
 //DEBUG
 window.just = just;
 
-void function initTextarea () {
-	just.plugin(function setCursorToEnd () {
+void
+function initTextarea() {
+	just.plugin(function setCursorToEnd() {
 		this.elements[0].selectionStart = this.elements[0].selectionEnd = this.elements[0].value.length;
 	});
 
-	just.plugin(function empty () {
+	just.plugin(function empty() {
 		just.select(this.elements[0].children).each(child => child.remove());
 		return just.select(this.elements[0]);
 	})
@@ -24,20 +27,55 @@ void function initTextarea () {
 	const input = just.select("#input");
 	const output = just.select("#output");
 
-	function markdownPreprocessor (md) {
-		const components = [
-			{
+	function markdownPreprocessor(md) {
+		const components = [{
 				name: "test",
 				value: `<p>Dit is een testcomponent, om te laten zien dat het component-system werkt.</p>`
+			},
+			{
+				name: "studentenwerk",
+				value: `<div class="studentenwerk__article">
+						<img
+							sizes="(max-width: 600px) 100vw, 600px"
+							srcset="../images/LR-kip-3-kopieren-small.jpg 320w,
+									../images/LR-kip-3-kopieren-medium.jpg 490w,
+									../images/LR-kip-3-kopieren-big.jpg 600w"
+							src="../images/LR-kip-3-kopieren-big.jpg"
+							alt="The game changer">
+						<h3>The game changer</h3>
+						<p>The Game Changer houdt de game-uren van uw kind bij. Als de tijd verstreken is kan uw kind niet meer spelen. Hoe eerder je stopt met gamen, hoe meer bonusuren je krijgt.</p>
+					</div>
+					<div class="studentenwerk__article">
+						<img
+							sizes="(max-width: 600px) 100vw, 600px"
+							srcset="../images/LR-kip-3-kopieren-small.jpg 320w,
+									../images/LR-kip-3-kopieren-medium.jpg 490w,
+									../images/LR-kip-3-kopieren-big.jpg 600w"
+							src="../images/LR-kip-3-kopieren-big.jpg"
+							alt="The game changer">
+						<h3>The game changer</h3>
+						<p>The Game Changer houdt de game-uren van uw kind bij. Als de tijd verstreken is kan uw kind niet meer spelen. Hoe eerder je stopt met gamen, hoe meer bonusuren je krijgt.</p>
+					</div>
+					<div class="studentenwerk__article">
+						<img
+							sizes="(max-width: 600px) 100vw, 600px"
+							srcset="../images/LR-kip-3-kopieren-small.jpg 320w,
+									../images/LR-kip-3-kopieren-medium.jpg 490w,
+									../images/LR-kip-3-kopieren-big.jpg 600w"
+							src="../images/LR-kip-3-kopieren-big.jpg"
+							alt="The game changer">
+						<h3>The game changer</h3>
+						<p>The Game Changer houdt de game-uren van uw kind bij. Als de tijd verstreken is kan uw kind niet meer spelen. Hoe eerder je stopt met gamen, hoe meer bonusuren je krijgt.</p>
+					</div>`
 			}
 		];
 		return md.replace(/#\{.+?\}/g, match => {
-			const name = match.match(/#\{(?<name>.+?)\}/)[1];
-			return (components.find(comp => comp.name === name)||{}).value||"";
+			const name = match.match(/#\{(.+?)\}/)[1];
+			return (components.find(comp => comp.name === name) || {}).value || "";
 		});
 	}
 
-	function HTMLPostprocessor (raw) {
+	function HTMLPostprocessor(raw) {
 		const elements = Array.from(new DOMParser().parseFromString(raw, "text/html").body.children);
 
 		return elements
@@ -52,7 +90,7 @@ void function initTextarea () {
 			});
 	}
 
-	function updateOutput () {
+	function updateOutput() {
 		const value = md.render(markdownPreprocessor(input.value));
 		output.empty();
 		HTMLPostprocessor(value)
@@ -69,7 +107,8 @@ void function initTextarea () {
 		.setCursorToEnd();
 }();
 
-void function initStatusbar () {
+void
+function initStatusbar() {
 	const statusbar = just.select("#statusbar");
 	const input = just.select("#input");
 	just.select("#publish")
@@ -78,7 +117,10 @@ void function initStatusbar () {
 			const raw = input.value;
 			const res = await just.http
 				.post("/save")
-				.json({ title: getFirstHeadingLinkSafe(raw), raw });
+				.json({
+					title: getFirstHeadingLinkSafe(raw),
+					raw
+				});
 
 			if (!res.ok)
 				return console.warn("Notify user of failure.", res.error);
